724380dd6603416d15b63acf2cca7ffd1bb078e9
Guangsheng Guan <111338977+guanguansheng@users.noreply.github.com>
2025-02-07 11:41:12 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
-# 训练参数官方详解链接：https://docs.ultralytics.com/modes/train/#resuming-interrupted-trainings:~:text=a%20training%20run.-,Train%20Settings,-The%20training%20settings
+
 
 if __name__ == '__main__':
     model = YOLO('ultralytics/cfg/models/mamba-yolo/Mamba-YOLO-B.yaml')
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                 # device='0',
                 optimizer='SGD', # using SGD
                 # patience=0, # close earlystop
-                # resume=True, # 断点续训,YOLO初始化时选择last.pt
+                # resume=True, 
                 # amp=False, # close amp
                 # fraction=0.2,
                 project='runs/trainmambaYOLO',
