4f12a8dcc1679c638271a071ef646e7a950136df
Guangsheng Guan <111338977+guanguansheng@users.noreply.github.com>
2025-02-07 11:41:41 +0800

    Update val.py

--- val.py
+++ val.py
@@ -2,7 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
-# 验证参数官方详解链接：https://docs.ultralytics.com/modes/val/#usage-examples:~:text=of%20each%20category-,Arguments%20for%20YOLO%20Model%20Validation,-When%20validating%20YOLO
+
 
 if __name__ == '__main__':
     model = YOLO('runs/train/18yolov8n-4RFCBAM-FDPN-TADDH/weights/best.pt')
